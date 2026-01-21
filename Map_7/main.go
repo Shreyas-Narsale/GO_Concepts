2c9ff0e79f2d77f74ab7befd27c6f11988429dfc
Shreyas Narsale <89839848+Shreyas-Narsale@users.noreply.github.com>
2026-01-21 12:05:28 +0530

    Update Map
    added map and sync.Map both

--- Map_7/main.go
+++ Map_7/main.go
@@ -1,23 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
+	mapDt := make(map[string]string) //allowed
+	//mapDt := make(map[string]int, 5) //allowed
 
-	FavData := make(map[string]string)
+	mapDt["key1"] = "hello"
+	mapDt["key2"] = "demo"
+	mapDt["key3"] = "from"
 
-	FavData["color"] = "red"
-	FavData["fruit"] = "mango"
-	FavData["sport"] = "cricket"
+	delete(mapDt, "key2")
 
-	fmt.Println(FavData)
-
-	delete(FavData, "color")
-
-	fmt.Println(FavData)
-
-	for key, value := range FavData {
-		fmt.Println("key:", key, ", value:", value)
+	for key, value := range mapDt {
+		fmt.Println("key:", key, ",Value:", value)
 	}
 
+	litMapDt := map[string]int{"a": 1, "b": 2} //allowed
+	fmt.Println("data[a]:", litMapDt["a"])
+
+	//nilMap := map[string]string // not allowed
+
+	var syncMap = sync.Map{}
+	syncMap.Store("key1", "value1")
+	syncMap.Store("key2", 10)
+
+	val, isContain := syncMap.Load("key1")
+	if isContain {
+		fmt.Println("key1:", val)
+	}
+
+	for key, value := range syncMap.Range {
+		fmt.Println("key:", key)
+		fmt.Println("value:", value)
+
+	}
 }
