868875842cf4543564a06d4dbc170f2937c4badb
Shreyas Narsale <89839848+Shreyas-Narsale@users.noreply.github.com>
2026-05-08 15:34:59 +0530

    Update 2.StrategyPattern.go

--- LLD/2.StrategyPattern.go
+++ LLD/2.StrategyPattern.go
@@ -9,6 +9,15 @@ define multiple algorithms/behaviors,
 switch obj dynamically at runtime.
 means: at runtime: like suppose at runtime in phonepay : switch payment pay option from icici cred card to Hdfc debit card and then pay.
 So there we set startrgey(context) at runtime and also chnages that startegy
+
+Advantages:
+At runtime:
+	object changes, behavior changes automatically.
+	Open/Closed Principle:
+		Add new strategy without modifying old code. ( no need to changes anything now even Startegry context, just easily add new one)
+
+Real-Time Uses Cases:
+Payment Gateway
 */
 
 type PaymentStrategy interface {
@@ -57,14 +66,4 @@ func main() {
 
 }
 
-/*Advantages:
-At runtime:
-	object changes, behavior changes automatically.
-	Open/Closed Principle:
-		Add new strategy without modifying old code. ( no need to changes anything now even Startegry context, just easily add new one)
-*/
 
-/*
-Real-Time Uses Cases:
-Payment Gateway
-*/
