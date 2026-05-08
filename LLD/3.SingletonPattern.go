b4814ab6a6ac2572d152d49bd7bef3dbd174f285
Shreyas Narsale <89839848+Shreyas-Narsale@users.noreply.github.com>
2026-05-08 14:18:11 +0530

    Create 3.SingletonPattern.go

---
+++ LLD/3.SingletonPattern.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+/* Singleton Pattern:( Creational Design Pattern)
+only one object(instance) is created
+and provides a global access point to it.
+Thread-Safe Singleton
+Use: sync.Once
+*/
+
+type DBConn struct {
+	connection string
+}
+
+var instance *DBConn
+
+func GetConnection() *DBConn {
+	if instance == nil {
+		instance = &DBConn{connection: "DB Connection!!!"}
+	}
+	return instance
+}
+
+func main() {
+	conn1 := GetConnection()
+	conn2 := GetConnection()
+
+	if conn1 == conn2 {
+		fmt.Println("Same Conn reference")
+	}
+
+}
+
+/*Advantages:
+1. Single Shared Resource
+	One config/logger/db manager.
+2. Saves Memory
+
+Disadvantage:
+1. Tight Coupling
+*/
+
+/*
+Real-Time Uses Cases:
+DB Connection
+*/
